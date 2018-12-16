6f09a30b6bc7c247a3a5d460333dd3d026598469
Troels Schwarz-Linnet <tlinnet@gmail.com>
2018-12-16 20:18:21 +0100

    Adding examples

--- pointsplane.py
+++ pointsplane.py
@@ -1,6 +1,6 @@
 '''
 Described at PyMOL wiki:
-https://pymolwiki.org/index.php/Plane_Wizard
+https://pymolwiki.org/index.php/Pointsplane
 
 Authors : Troels Schwarz-Linnet
 Date    : Dec 2018
@@ -11,9 +11,16 @@ import numpy as np
 import pymol
 from pymol import cmd
 from chempy import cpv
-from pymol.cgo import COLOR, SPHERE, CYLINDER, BEGIN, TRIANGLE_STRIP, NORMAL, VERTEX, END, ALPHA
+from pymol.cgo import COLOR, SPHERE, CYLINDER, BEGIN, TRIANGLE_STRIP, TRIANGLE_FAN, NORMAL, VERTEX, END, ALPHA
 
-def test(sele=''):
+def makePrimitive(cgo, name):
+    az = cmd.get('auto_zoom', quiet=1)
+    cmd.set('auto_zoom', 0, quiet=1)
+    cmd.load_cgo(cgo, name)
+    cmd.set('auto_zoom', az, quiet=1)
+
+
+def get_xs_ys_zs(sele=''):
     coor = cmd.get_model(sele).get_coord_list()
     
     xs = []
@@ -24,8 +31,60 @@ def test(sele=''):
         ys.append(i[1])
         zs.append(i[2])
 
-    print(xs)
-    print(ys)
-    print(zs)
+    return xs, ys, zs
 
-cmd.extend("test", test)
+def do_fit(xs, ys, zs):
+    tmp_A = []
+    tmp_b = []
+
+    for i in range(len(xs)):
+        tmp_A.append([xs[i], ys[i], 1])
+        tmp_b.append(zs[i])
+    b = np.matrix(tmp_b).T
+    A = np.matrix(tmp_A)
+    fit = (A.T * A).I * A.T * b
+    errors = b - A * fit
+    residual = np.linalg.norm(errors)
+    #print("solution:")
+    #print("%f x + %f y + %f = z" % (fit[0], fit[1], fit[2]))
+    abc = [float(fit[0]), float(fit[1]), float(fit[2])]
+    return abc
+
+def plane(sele='', normal=None, settings=None):
+    planeObj = []
+
+    # Make settings
+    if 'ALPHA' in settings:
+        planeObj.extend([ALPHA, settings['ALPHA']])
+
+    if 'COLOR' in settings:
+        planeObj.extend([COLOR, settings['COLOR'][0], settings['COLOR'][1], settings['COLOR'][2]])
+    else:
+        planeObj.extend([COLOR, 0.8, 0.8, 0.8]) # greyish
+
+    #planeObj.extend([BEGIN, TRIANGLE_STRIP])
+    planeObj.extend([BEGIN, TRIANGLE_FAN])
+    planeObj.append(NORMAL)
+
+    if 'INVERT' in settings:
+        if settings['INVERT']==True:
+            planeObj.extend(cpv.negate(normal))
+        else:
+            planeObj.extend(normal)
+    else:
+        planeObj.extend(normal)
+
+    coor = cmd.get_model(sele).get_coord_list()
+    for corner in coor:
+        planeObj.append(VERTEX)
+        planeObj.extend(corner)
+    planeObj.append(END)
+    return planeObj
+
+def make_points_plane(name="Myplane", sele='', settings={}):
+    xs, ys, zs = get_xs_ys_zs(sele=sele)
+    abc = do_fit(xs, ys, zs)
+    pplane = plane(sele=sele, normal=[0., 0., 1.], settings=settings)
+    makePrimitive(pplane, name)
+
+cmd.extend("make_points_plane", make_points_plane)
--- pymol.pml
+++ pymol.pml
@@ -1,22 +1,17 @@
 reinitialize
+bg_color white
 
 import pointsplane
-#import plane
+import plane
 
 load Heteroaromatics/Pyridine.xyz
 select pyr, Pyridine and not name H
 
-pointsplane.test("pyr")
+dict = {'ALPHA':0.6, 'COLOR':[0.55, 0.25, 0.60], 'INVERT':False}
+pointsplane.make_points_plane(name="pyr plane", sele="pyr", settings=dict)
 
-# Start the wizard, and do manual picking
-#cmd.set_wizard(plane.PlaneWizard())
+load Heteroaromatics/Benzofuran.xyz
+select benz, Benzofuran and not name H
 
-#plane.make_plane_points(name='plane-00', l1=[-0.67, 1.18, 0.0], l2=[-1.39, -0.01, 0.0], l3=[-0.69, -1.22, 0.0])
-#plane.make_plane_points(name='plane-01', l1=[-0.67, 1.18, 0.0], l2=[-1.39, -0.01, 0.0], l3=[-0.69, -1.22, 0.0], center=False)
-#plane.make_plane_points(name='plane-02', l2=[-0.67, 1.18, 0.0], l3=[-1.39, -0.01, 0.0], l1=[-0.69, -1.22, 0.0], center=False)
-#plane.make_plane_points(name='plane-03', l3=[-0.67, 1.18, 0.0], l1=[-1.39, -0.01, 0.0], l2=[-0.69, -1.22, 0.0], center=False)
-
-
-#dict = {'ALPHA':0.6, 'COLOR':[0.55, 0.25, 0.60], 'INVERT':True}
-#plane.make_plane_points(name='plane-02', l1=[-0.67, 1.18, 0.0], l2=[-1.39, -0.01, 0.0], l3=[-0.69, -1.22, 0.0], center=False, settings=dict)
-
+dict2 = {'ALPHA':0.6, 'COLOR':[0.0, 0.0, 1.0], 'INVERT':False}
+pointsplane.make_points_plane(name="benz plane", sele="benz", settings=dict2)
